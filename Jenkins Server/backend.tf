cbf088e5bade699e14e8e86fb13b7ec22c2d428c
Anuj Pandey <vky00000@gmail.com>
2025-06-10 12:30:32 +0530

    commit on 10/06/2025

--- Jenkins Server/backend.tf
+++ Jenkins Server/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "cicd-terraform-eks"
+    bucket = "cicd-eks-terraform-project"
     key    = "jenkins/terraform.tfstate"
     region = "us-east-1"
   }
--- Jenkins Server/jenkins-install.sh
+++ Jenkins Server/jenkins-install.sh
@@ -1,25 +1,35 @@
-#!/bin/bash
+#!/bin/bash -xe
 
-# install jenkins
+# Update system
 sudo yum update -y
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade -y
-sudo amazon-linux-extras install java-openjdk11 -y
-sudo yum install jenkins -y
+
+# Add Jenkins repo and install Jenkins
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+# Install Java 17 (Amazon Corretto), Jenkins, and start Jenkins
+sudo yum install -y java-17-amazon-corretto jenkins
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
+sudo systemctl status jenkins
 
-# then install git
-sudo yum install git -y
+# Install Git
+sudo yum install -y git
 
-#then install terraform
+# Install Terraform
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
+sudo yum install -y terraform
 
-#finally install kubectl
-sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
-sudo chmod +x ./kubectl
-sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+# Install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin
+cp ./kubectl $HOME/bin/kubectl
+
+# Add $HOME/bin to PATH for the current session
+export PATH=$PATH:$HOME/bin
+
+# Optionally add it to .bashrc for future sessions
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
